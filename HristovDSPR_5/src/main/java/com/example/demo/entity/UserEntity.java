b275fd9a8c0bac64f86980196e621373ece374bb
Dima181 <117594480+Dima181@users.noreply.github.com>
2024-12-19 23:15:19 +0300

    5Lab

---
+++ HristovDSPR_5/src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String job;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
